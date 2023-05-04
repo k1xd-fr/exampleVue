de0290a26bb3461e65f26c3e0a8c1eefed8a90a8
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-04 18:12:11 +0600

    fix problem

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'exampleVue',
+  base: '/exampleVue',
   plugins: [vue()],
   resolve: {
     alias: {
