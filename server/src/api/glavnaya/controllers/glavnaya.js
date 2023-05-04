8fff53a9bde7e675be3026cc124e2b1028e3c11a
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-04 15:46:29 +0600

    change

--- server/src/api/glavnaya/controllers/glavnaya.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * glavnaya controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::glavnaya.glavnaya');
--- server/src/api/glavnaya/routes/glavnaya.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * glavnaya router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::glavnaya.glavnaya');
